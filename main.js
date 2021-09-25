45c14679317d2eab9bc0555e5716a9edfa9ad7e1
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-09-24 20:50:31 -0400

    media style.css and pop up js footer

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //https://teachablemachine.withgoogle.com/models/XMCwG462D/model.json
+
+function startClassification() {
+    navigator.mediaDevices.getDisplayMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/XMCwG462D/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
